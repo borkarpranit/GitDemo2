81d9cc6551add4f4ed723998a7d0deba3eb4759a
pranit <borkarpranit@gmail.com>
2025-12-19 14:52:21 +0530

    2nd commit

--- Welcome/src/com/app/Msg.java
+++ Welcome/src/com/app/Msg.java
@@ -2,6 +2,6 @@ package com.app;
 
 public class Msg {
 	public static void main(String[] args) {
-		System.out.println("CJC Karvenagar");
+		System.out.println("CJC Karvenagar |||||||||||||||||||||||||||||");
 	}
 }
