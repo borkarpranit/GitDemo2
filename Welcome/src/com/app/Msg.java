f6dd2f893d57686d2a22b75c230adee61633dafa
Pranit Borkar <100359396+borkarpranit@users.noreply.github.com>
2025-12-19 15:47:19 +0530

    Update Msg.java
    commit again

--- Welcome/src/com/app/Msg.java
+++ Welcome/src/com/app/Msg.java
@@ -2,6 +2,7 @@ package com.app;
 
 public class Msg {
 	public static void main(String[] args) {
-		System.out.println("CJC Karvenagar ");
+		System.out.println("CJC Karvenagar");
+		System.out.println("CJC Pune");
 	}
 }
